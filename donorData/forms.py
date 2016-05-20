f73bfd09f66e5c820b933dad0d2740d0f42e9d48
= <=>
2016-05-20 14:54:11 -0700

    New donor form looks ok but won't post yet

--- donorData/forms.py
+++ donorData/forms.py
@@ -1,11 +1,18 @@
-from .models import Donor
-from django.forms import ModelForm
+from .models import Donor, Patronage
+from django.forms import ModelForm, modelformset_factory, inlineformset_factory
 
+
+donorFields = [
+    'first_name', 'last_name', 'job_title',
+    'employer_url', 'employer_industry'
+]
+
+DonorFormSet = inlineformset_factory(Donor, Patronage, fields=['name'])
 
 class DonorForm(ModelForm):
     class Meta:
         model = Donor
-        fields = [
-            'first_name', 'last_name', 'job_title',
-            'employer_url', 'employer_industry'
-        ]
+        fields = donorFields
+
+
+PatronageFormSet = modelformset_factory(Patronage, fields=['name'], extra=3)
--- donorData/views.py
+++ donorData/views.py
@@ -1,7 +1,7 @@
 from django.shortcuts import render
 from django.http import HttpResponseRedirect, Http404
 from django.views import generic
-from .forms import DonorForm
+from .forms import DonorForm, DonorFormSet, PatronageFormSet
 from .models import Donor
 
 
@@ -11,15 +11,40 @@ class IndexView(generic.ListView):
 
 
 def new(request):
-    if request.method == 'POST':
-        form = DonorForm(request.POST)
-        if form.is_valid():
-            form.save()
-        return HttpResponseRedirect('/')
-    else:
-        form = DonorForm()
-        return render(request, 'donorData/donorDetail.html', {'form': form})
 
+    #LineItemFormSet = inlineformset_factory(Invoice, LineItem, form=LineItemForm, extra=5)
+    if request.method == 'POST':
+        # invoice_form = InvoiceForm(request.POST, prefix='invoice')
+        # lineitem_formset = LineItemFormSet(request.POST, request.FILES, prefix='line')
+        # if invoice_form.is_valid() and lineitem_formset.is_valid():
+        #     invoice = invoice_form.save()
+        #     # I recreate my lineitem_formset bound to the new invoice instance
+        #     lineitem_formset = LineItemFormSet(request.POST, request.FILES, prefix='line', instance=invoice)
+        #     # I have to validate (again - so I'm confident) to access clean data
+        #     lineitem_formset.is_valid()
+        #     lineitem_formset.save()
+        #     if 'submit_more' in request.POST:
+        #         return HttpResponseRedirect(reverse('invoices:add_invoice'))
+        #     else:
+        #         return HttpResponseRedirect(reverse('invoices:get_invoices'))
+        # else:
+        #     return render(request, 'invoices/invoice_add.html', {
+        #         'message'           : "Check your form",
+        #         'invoice_form'      : invoice_form,
+        #         'lineitem_formset'  : lineitem_formset,
+        #     })
+        pass
+    else:
+        donor_form = DonorForm(prefix='donor')
+        patronage_formset = PatronageFormSet(prefix='patronage')
+        print('donorForm')
+        print(donor_form)
+        print('patronage')
+        print(patronage_formset)
+        return render(request, 'donorData/donorDetail.html', {
+            'donor_form': donor_form,
+            'patronage_formset': patronage_formset,
+        })
 
 def detail(request, pk):    
     if request.method == 'POST':
@@ -28,7 +53,7 @@ def detail(request, pk):
         except:
             return Http404('Donor does not exist')
         else:
-            form = DonorForm(request.POST, instance=donorInstance)
+            form = DonorFormSet(request.POST, instance=donorInstance)
             if form.is_valid():
                 form.save()
             return HttpResponseRedirect('/')
